c7eba6389bcdec94cb0b99a1f672e61d75f4ae5b
Ngori Januário <68328526+ngorijanuario@users.noreply.github.com>
2020-10-20 05:18:57 +0100

    a1

--- main.dart
+++ main.dart
@@ -1,6 +1,11 @@
-import 'dart:html';
+import  'dart:html' ;
 
-void main() {
-  var header = querySelector('#header');
-  header.text = "FAURA Academy";
+void  main () {
+  var header =  querySelector ( '#header' );
+  var paragrafo = querySelector('p');
+  var barra = querySelector('.barra');
+  
+  header.text =  "Faura Academy" ;
+  paragrafo.text = '"Criando um futuro melhor"';
+  barra.text = "Todos os Direitos reservados - Ngori Januário";
 }
